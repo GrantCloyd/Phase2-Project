55ee4d49b6086dbdb592f55903e97f7154376fa2
Anchobies <anthonychung97@gmail.com>
2021-06-23 13:24:44 -0400

    fixed date

--- src/Components/Featured.js
+++ src/Components/Featured.js
@@ -8,14 +8,16 @@ export default function Featured() {
    const myDate = new Date()
    let myDateString = ""
 
-   myDate.setDate(myDate.getDate() + 20)
+   myDate.setDate(myDate.getDate())
 
    myDateString =
       myDate.getFullYear() +
       "-" +
       ("0" + (myDate.getMonth() + 1)).slice(-2) +
       "-" +
-      ("0" + myDate.getDate()).slice(-2)
+      ("0" + myDate.getDate()).slice(-2);
+
+   console.log(myDateString);
 
    let featuredArray = featured.map(featureItem => {
       return (
